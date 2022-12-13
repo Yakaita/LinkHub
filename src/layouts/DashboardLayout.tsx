7f2ed2e3e982ab58f7953309a74209afd14c859d
Kiran Hart <kiran.hart@live.com>
2022-12-13 18:28:35 -0500

    📏 More or less implement a dashboard layout

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
-const DashboardLayout: React.FC = () => {
-    return <div>DashboardLayout</div>;
+interface DashboardLayoutProps {
+    children?: React.ReactNode;
+}
+
+const DashboardLayout: React.FC = ({ children }: DashboardLayoutProps) => {
+    return (
+        <div className="pb-4">
+            <h3 className="pt-8 pb-2 font-satoshi text-3xl font-semibold text-white">
+                Dashboard
+            </h3>
+            <hr className="mb-8 opacity-20" />
+            {children}
+        </div>
+    );
 };
 
 export default DashboardLayout;
