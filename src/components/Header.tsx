5524f0ab03e8aa7797d7307ee126919727e41a50
Kiran Hart <kiran.hart@live.com>
2022-12-13 18:29:23 -0500

    ⛑️ header is using #000 as bg, add user profile image, and feedback/dash links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,28 @@
+import { useSession } from 'next-auth/react';
 import Link from 'next/link';
 import React from 'react';
 
 const Header: React.FC = () => {
+    const { data: session } = useSession();
+
     return (
-        <header className="sticky flex items-center justify-center bg-[#1D1D1D] py-4 pl-12 pr-4 font-inter font-black ">
-            <h2 className="text-center text-2xl text-white md:text-left">
-                LinkHub
-            </h2>
+        <header className="sticky flex items-center justify-between border-b-[1px] border-gray-50/25 bg-black py-4 px-12 font-inter font-black ">
+            <h2 className="text-left text-2xl text-white">LinkHub</h2>
+            <div className="flex flex-row-reverse items-center gap-8">
+                <img
+                    src={session?.user?.image}
+                    alt="Profile Image"
+                    className="w-12 rounded-full"
+                />
+                <ul className="flex items-center gap-4 font-satoshi font-semibold text-white/75">
+                    <li className="rounded-md border-[1px] border-gray-50/25 px-4 py-[4px]">
+                        <Link href="/feedback">Feedback</Link>
+                    </li>
+                    <li className="">
+                        <Link href="/dashboard">Dashboard</Link>
+                    </li>
+                </ul>
+            </div>
         </header>
     );
 };
