fef6110d32972722c0e92c5bdaf36885010b2764
Kiran Hart <kiran.hart@live.com>
2022-12-13 18:27:14 -0500

    🦶 add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+    return (
+        <footer className="flex items-center justify-center font-inter font-semibold tracking-wide text-gray-600">
+            <h3>Designed By Kiran Hart</h3>
+        </footer>
+    );
+};
+
+export default Footer;
